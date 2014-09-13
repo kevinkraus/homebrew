13b030d82b80f86330f2366630a1877eb97f95fb
Kevin Kraus <kraus.kevin@gmail.com>
2014-09-13 17:26:34 -0500

    Don't overwrite existing Ruby installs

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,5 +8,6 @@ ln -s "/Users/kraus/Documents/homebrew/.bash_profile" "$HOME/.bash_profile"
 brew bundle
 
 #Use rbenv to install Ruby
-rbenv install 2.1.2
+#Don't overwrite Ruby if it's already installed
+n | rbenv install 2.1.2
 rbenv global 2.1.2
